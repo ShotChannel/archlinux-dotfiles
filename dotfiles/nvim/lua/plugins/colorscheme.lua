34eec1437ef0d2b610c0e539b3419fea3746c852
ShotChannel (love Hatsune Miku) <anp64834@gmail.com>
2026-02-14 23:22:48 +0700

    add nvim files

---
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin", -- Set this to 'catppuccin'
+    },
+  },
+  -- You can also add options for catppuccin here:
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- Example flavor option
+    },
+  },
+}
